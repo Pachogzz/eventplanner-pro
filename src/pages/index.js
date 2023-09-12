e8463899ed328e52d6dc29f3893d7b48c3df037a
Pacho Gonzalez <pachogzz@gmail.com>
2023-09-11 19:23:35 -0600

    Creating blog with his query

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import { header } from "../styles/home.module.css"
 import { btn } from "../styles/home.module.css"
 
 export default function Home() {
+
   return (
     <Layout>
       <section className={header}>
@@ -12,8 +13,9 @@ export default function Home() {
           <h2>Home</h2>
           <h3>Event Planner Pro</h3>
           <p>Lorem Ipsum</p>
-          <Link className={btn}>Read the Blog</Link>
+          <Link to="/blog" className={btn}>Read the Blog</Link>
         </div>
+        <img src="/banner.png" alt="Site banner" style={{maxWidth: '100%'}} />
       </section>
     </Layout>
   )
